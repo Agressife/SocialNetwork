253239ec8d7ed2ca434092cda5c44c2301463c1a
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:11:12 +0700

    Создан класс пост пользователя.

---
+++ SocialNetwork/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    class Post:IPost
+    {
+     public int Id { get; set; }
+        public IMember Member { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
+
+    }
+}
