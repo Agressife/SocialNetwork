69e84cd19336d817ed95649ca489de4e5b00243e
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:22:47 +0700

    создан класс пользователь.

---
+++ SocialNetwork/Member.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    class Member:IMember
+    {
+        public int Id { get; }
+        public IMemberProfile Profile { get; }
+        public IEnumerable<IMember> Friends { get { return null; } }
+        public IEnumerable<IMember> Pending { get { return null; } }
+        public IEnumerable<IPost> Posts { get { return null; } }
+        public void AddFriendRequest(IMember from)
+        {            
+        }
+        public void ConfirmFriend(IMember member)
+        {
+
+        }
+        public void RemoveFriendRequest(IMember member)
+        {
+
+        }
+        public void RemoveFriend(IMember friend)
+        {
+
+        }
+        public IEnumerable<IMember> GetFriends(int level = 1,IList<int> fliter=null)
+        {
+            return null;
+        }
+        public IPost AddPost(string message)
+        {
+            return null;
+        }
+        public void RemovePost(int id)
+        {
+
+        }
+        public IEnumerable<IPost> GetFeed()
+        {
+            return null;
+        }
+    }
+}
