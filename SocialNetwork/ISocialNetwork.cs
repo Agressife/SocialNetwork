8ffe7167467e2d7fed6ef2ed5bcd3745a87c6cea
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 16:53:45 +0700

    Создан интерфейс для социальной сети

---
+++ SocialNetwork/ISocialNetwork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    interface ISocialNetwork
+    {
+        public IMember AddMember(string name1, string name2, string city, string country);
+        public IMember FindMemberById(int id);
+        public IEnumerable<IMember> FindMember(string search);
+        public int MemberCount { get { return 0; } }
+    }
+}
