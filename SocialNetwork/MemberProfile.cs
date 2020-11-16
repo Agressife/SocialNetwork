bfb3f48a9918a1d5d33ad42dd80db499e5669222
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:12:47 +0700

    Создан класс профиль пользователя.

---
+++ SocialNetwork/MemberProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    class MemberProfile:IMemberProfile
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+    }
+}
