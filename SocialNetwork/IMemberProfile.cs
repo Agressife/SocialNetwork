a8777eb809dd541fea8a0ff6b562a85700ed71b1
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:05:10 +0700

    создан интерфейс для профиля пользователя/

---
+++ SocialNetwork/IMemberProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    interface IMemberProfile
+    {
+        public int MemberId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set;}
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
