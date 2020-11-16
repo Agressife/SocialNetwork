d7d9297d4788e8877737687257e66954019e9251
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:06:53 +0700

    создан интерфейс для постов.

---
+++ SocialNetwork/IPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    interface IPost
+    {
+        public int Id { get; set; }
+        public IMember Member { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public int Likes { get; set; }
+    }
+}
