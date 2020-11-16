103ca82000b195e5d11082fa84adbc2d76103de1
Alex Vorobyev <73647832+Agressife@users.noreply.github.com>
2020-11-16 17:26:54 +0700

    Создан класс социальная сеть.

---
+++ SocialNetwork/SocialNetwork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialNetwork
+{
+    class SocialNetwork:ISocialNetwork
+    {
+        public IMember AddMember(string firstName,string lastName,string city,string country)
+        {
+            return null;
+        }
+        public IMember FindMemberById(int id)
+        {
+            return null;
+        }
+        public IEnumerable<IMember> FindMember(string search)
+        {
+            return null;
+        }
+        public int MemberCount { get { return 0; } }
+    }
+}
